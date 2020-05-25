45d806c31cfc973353f23a6067a330720480ad6c
Left.Twix.Wand <0681813464.dev@gmail.com>
2020-05-25 13:05:21 +0300

    Added Server project, libraries for grains and interfaces.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,26 +1,45 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Orleans.Configuration;
+using Orleans.Hosting;
 
 namespace Server
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            return new HostBuilder()
+                .UseOrleans(builder =>
+                {
+                    builder
+                        .Configure<ClusterOptions>(options =>
+                        {
+                            options.ClusterId = "dev";
+                            options.ServiceId = "HelloWorldApp";
+                        })
+                        .Configure<EndpointOptions>(options =>
+                        {
+                            options.AdvertisedIPAddress = IPAddress.Loopback;
+                            //options.SiloPort = 11112;
+                            //options.GatewayPort = 30001;
+                        });
+                 })
+                .RunConsoleAsync();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+    //    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    //        Host.CreateDefaultBuilder(args)
+    //            .ConfigureWebHostDefaults(webBuilder =>
+    //            {
+    //                webBuilder.UseStartup<Startup>();
+    //            });
     }
 }
